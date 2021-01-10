377e3dd81aeb82567483f62b00e1ef8e284bd241
Max Bickers <mbickers@umich.edu>
2021-01-10 14:06:07 -0500

    Configured build script to remove resume html file

--- build.sh
+++ build.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 hugo -D
 /Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome --headless --disable-gpu --run-all-compositor-stages-before-draw --print-to-pdf=public/MaxBickersResume.pdf --virtual-time-budget=6000 public/resume/index.html
+rm -r public/resume
