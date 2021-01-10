f2729caa05cf006682264ae7518a00fe3f94cf46
Max Bickers <mbickers@umich.edu>
2021-01-10 14:51:11 -0500

    Added OS handling to build script

--- build.sh
+++ build.sh
@@ -1,4 +1,10 @@
 #!/bin/sh
 hugo --cleanDestinationDir
-/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome --headless --disable-gpu --run-all-compositor-stages-before-draw --print-to-pdf=public/MaxBickersResume.pdf --virtual-time-budget=6000 public/resume/index.html
+
+if [[ "$OSTYPE" == "darwin"* ]]; then
+    /Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome --headless --disable-gpu --run-all-compositor-stages-before-draw --print-to-pdf=public/MaxBickersResume.pdf --virtual-time-budget=6000 public/resume/index.html
+else
+    chrome --headless --disable-gpu --run-all-compositor-stages-before-draw --print-to-pdf=public/MaxBickersResume.pdf --virtual-time-budget=6000 public/resume/index.html
+fi
+
 rm -r public/resume
