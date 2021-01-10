056aad923bc00afe4d7fea2ebb84fe4a8872dc4b
Max Bickers <mbickers@umich.edu>
2021-01-10 14:18:07 -0500

    Removed RSS feed info from build

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-hugo -D
+hugo --cleanDestinationDir
 /Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome --headless --disable-gpu --run-all-compositor-stages-before-draw --print-to-pdf=public/MaxBickersResume.pdf --virtual-time-budget=6000 public/resume/index.html
 rm -r public/resume
