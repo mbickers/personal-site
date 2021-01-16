837244e2038fa2db9e31f2e7705bf82020e29c95
Max Bickers <mbickers@umich.edu>
2021-01-15 22:44:06 -0500

    Added chrome installation to build script

--- build.sh
+++ build.sh
@@ -1,10 +1,23 @@
 #!/bin/sh
-hugo --cleanDestinationDir
+set -eou pipefail
 
+# Check if building locally or on Netlify
 if [[ "$OSTYPE" == "darwin"* ]]; then
-    /Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome --headless --disable-gpu --run-all-compositor-stages-before-draw --print-to-pdf=public/MaxBickersResume.pdf --virtual-time-budget=6000 public/resume/index.html
+    CHROME_BIN=/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome
+    if [ ! -f "$CHROME_BIN" ]; then
+        echo "Building on macOS requires standard Chrome installation"
+        exit 2
+    fi
 else
-    chrome --headless --disable-gpu --run-all-compositor-stages-before-draw --print-to-pdf=public/MaxBickersResume.pdf --virtual-time-budget=6000 public/resume/index.html
+    # Install Chrome
+    wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+    mkdir chrome
+    dpkg -x google-chrome-stable_current_amd64.deb chrome
+    rm google-chrome-stable_current_amd64.deb
+    CHROME_BIN="chrome/opt/google/chrome/google-chrome"
 fi
 
+hugo --cleanDestinationDir
+
+"$CHROME_BIN" --headless --disable-gpu --run-all-compositor-stages-before-draw --print-to-pdf=public/MaxBickersResume.pdf --virtual-time-budget=6000 public/resume/index.html
 rm -r public/resume
